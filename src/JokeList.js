5856073e4b2f7f2b284aca2727ef80d389420287
Jerico James Gantuangco <jgantuangco@yondu.com>
2024-02-19 15:09:02 +0800

    added jokelist functionality and css

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import "./JokeList.css";
+import Joke from "./Joke";
 
 function JokeList({ numJokesToGet = 10 }) {
   const [jokes, setJokes] = useState([]);
@@ -11,18 +14,44 @@ function JokeList({ numJokesToGet = 10 }) {
         let response = await axios.get("https://icanhazdadjoke.com/", {
           headers: { Accept: "application/json" },
         });
-        jokes.push(response.data.joke);
+        jokes.push({ id: uuidv4(), text: response.data.joke, votes: 0 });
       }
       setJokes(jokes);
     }
     loadJokes();
   }, [numJokesToGet]);
+
+  function handleVote(id, delta) {
+    setJokes(
+      jokes.map((joke) => {
+        if (joke.id === id) {
+          return { ...joke, votes: joke.votes + delta };
+        }
+        return joke;
+      })
+    );
+  }
   return (
     <div className="JokeList">
-      <h1>Joke List</h1>
-      <div className="JokeList-joke">
+      <div className="JokeList-sidebar">
+        <h1 className="JokeList-title">
+          <span>Joke</span> List
+        </h1>
+        <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+        <button className="JokeList-getmore">New Jokes</button>
+      </div>
+
+      <div className="JokeList-jokes">
         {jokes.map((joke) => (
-          <div>{joke}</div>
+          <div>
+            <Joke
+              key={joke.id}
+              votes={joke.votes}
+              text={joke.text}
+              upVote={() => handleVote(joke.id, 1)}
+              downVote={() => handleVote(joke.id, -1)}
+            />
+          </div>
         ))}
       </div>
     </div>
