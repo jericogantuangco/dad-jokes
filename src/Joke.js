ac9a7aba11426a5b0caccbdac186d40ee5500820
Jerico James Gantuangco <jgantuangco@yondu.com>
2024-02-23 13:35:28 +0800

    final touches to the app

--- src/Joke.js
+++ src/Joke.js
@@ -1,12 +1,126 @@
+import "./Joke.css";
+
+class Resource {
+  resourceType;
+
+  constructor(resourceType) {
+    this.forRollingOnFloor = this.forRollingOnFloor.bind(this);
+    this.forLaughing = this.forLaughing.bind(this);
+    this.forSmiley = this.forSmiley.bind(this);
+    this.forSlightlySmiling = this.forSlightlySmiling.bind(this);
+    this.forNeutral = this.forNeutral.bind(this);
+    this.forConfused = this.forConfused.bind(this);
+    this.forAngry = this.forAngry.bind(this);
+    this.resourceType = resourceType;
+  }
+
+  static type(resourceType) {
+    this.resourceType = resourceType;
+    if (resourceType === "color") {
+      return new Resource("color");
+    }
+    return new Resource();
+  }
+
+  forRollingOnFloor() {
+    if (this.resourceType === "color") {
+      return "#4CAF50";
+    }
+    return "em em-rolling_on_the_floor_laughing";
+  }
+
+  forLaughing() {
+    if (this.resourceType === "color") {
+      return "#8BC34A";
+    }
+    return "em em-laughing";
+  }
+
+  forSmiley() {
+    if (this.resourceType === "color") {
+      return "#CDDC39";
+    }
+    return "em em-smiley";
+  }
+
+  forSlightlySmiling() {
+    if (this.resourceType === "color") {
+      return "#FFEB3B";
+    }
+
+    return "em em-slightly_smiling_face";
+  }
+
+  forNeutral() {
+    if (this.resourceType === "color") {
+      return "#FFC107";
+    }
+
+    return "em em-neutral_face";
+  }
+
+  forConfused() {
+    if (this.resourceType === "color") {
+      return "#FF9800";
+    }
+    return "em em-confused";
+  }
+
+  forAngry() {
+    if (this.resourceType === "color") {
+      return "#F44336";
+    }
+    return "em em-angry";
+  }
+}
+
 function Joke({ votes, text, upVote, downVote }) {
+  function getColor() {
+    if (votes >= 15) {
+      return Resource.type("color").forRollingOnFloor();
+    } else if (votes >= 12) {
+      return Resource.type("color").forLaughing();
+    } else if (votes >= 9) {
+      return Resource.type("color").forSmiley();
+    } else if (votes >= 6) {
+      return Resource.type("color").forSlightlySmiling();
+    } else if (votes >= 3) {
+      return Resource.type("color").forNeutral();
+    } else if (votes >= 0) {
+      return Resource.type("color").forConfused();
+    }
+    return Resource.type("color").forAngry();
+  }
+
+  function getEmoji() {
+    if (votes >= 15) {
+      return Resource.type().forRollingOnFloor();
+    } else if (votes >= 12) {
+      return Resource.type().forLaughing();
+    } else if (votes >= 9) {
+      return Resource.type().forSmiley();
+    } else if (votes >= 6) {
+      return Resource.type().forSlightlySmiling();
+    } else if (votes >= 3) {
+      return Resource.type().forNeutral();
+    } else if (votes >= 0) {
+      return Resource.type().forConfused();
+    }
+    return Resource.type().forAngry();
+  }
   return (
     <div className="Joke">
       <div className="Joke-buttons">
         <i className="fas fa-arrow-up" onClick={upVote}></i>
-        <span>{votes}</span>
+        <span className="Joke-votes" style={{ borderColor: getColor() }}>
+          {votes}
+        </span>
         <i className="fas fa-arrow-down" onClick={downVote}></i>
       </div>
       <div className="Joke-text">{text}</div>
+      <div className="Joke-smiley">
+        <i className={getEmoji()} />
+      </div>
     </div>
   );
 }
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,36 +1,83 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import "./JokeList.css";
 import Joke from "./Joke";
 
 function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
+  const [jokes, setJokes] = useState(
+    JSON.parse(window.localStorage.getItem("jokes") ?? "[]")
+  );
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function loadJokes() {
-      let jokes = [];
-      while (jokes.length < numJokesToGet) {
+  const seenJokes = useMemo(
+    () => new Set(jokes.map((joke) => joke.text)),
+    [jokes]
+  );
+
+  const loadJokes = useCallback(async () => {
+    try {
+      let loadedJokes = [];
+      while (loadedJokes.length < numJokesToGet) {
         let response = await axios.get("https://icanhazdadjoke.com/", {
           headers: { Accept: "application/json" },
         });
-        jokes.push({ id: uuidv4(), text: response.data.joke, votes: 0 });
-      }
-      setJokes(jokes);
-    }
-    loadJokes();
-  }, [numJokesToGet]);
-
-  function handleVote(id, delta) {
-    setJokes(
-      jokes.map((joke) => {
-        if (joke.id === id) {
-          return { ...joke, votes: joke.votes + delta };
+        const newJoke = response.data.joke;
+        if (seenJokes.has(newJoke)) {
+          console.log("FOUND DUPLICATE");
+          console.log(newJoke);
+        } else {
+          loadedJokes.push({ id: uuidv4(), text: newJoke, votes: 0 });
         }
-        return joke;
-      })
+      }
+      setJokes([...jokes, ...loadedJokes]);
+      setLoading(false);
+    } catch (e) {
+      alert(e);
+      setLoading(false);
+    }
+  }, [seenJokes, jokes, numJokesToGet]);
+
+  useEffect(() => {
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+  }, [jokes, loadJokes]);
+
+  useEffect(() => {
+    if (jokes.length === 0) loadJokes();
+  }, [jokes, loadJokes]);
+
+  const handleVote = useCallback(
+    (id, delta) => {
+      setJokes(
+        jokes.map((joke) => {
+          if (joke.id === id) {
+            return { ...joke, votes: joke.votes + delta };
+          }
+          return joke;
+        })
+      );
+    },
+    [jokes]
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+  }, [jokes, handleVote]);
+
+  const handleClick = () => {
+    setLoading(true);
+    loadJokes();
+  };
+
+  if (loading) {
+    return (
+      <div className="JokeList-spinner">
+        <i className="far fa-8x fa-laugh fa-spin" />
+        <h1 className="JokeList-title">Loading...</h1>
+      </div>
     );
   }
+
   return (
     <div className="JokeList">
       <div className="JokeList-sidebar">
@@ -38,21 +85,25 @@ function JokeList({ numJokesToGet = 10 }) {
           <span>Joke</span> List
         </h1>
         <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-        <button className="JokeList-getmore">New Jokes</button>
+        <button className="JokeList-getmore" onClick={handleClick}>
+          New Jokes
+        </button>
       </div>
 
       <div className="JokeList-jokes">
-        {jokes.map((joke) => (
-          <div>
-            <Joke
-              key={joke.id}
-              votes={joke.votes}
-              text={joke.text}
-              upVote={() => handleVote(joke.id, 1)}
-              downVote={() => handleVote(joke.id, -1)}
-            />
-          </div>
-        ))}
+        {jokes
+          .sort((a, b) => b.votes - a.votes)
+          .map((joke) => (
+            <div>
+              <Joke
+                key={joke.id}
+                votes={joke.votes}
+                text={joke.text}
+                upVote={() => handleVote(joke.id, 1)}
+                downVote={() => handleVote(joke.id, -1)}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
